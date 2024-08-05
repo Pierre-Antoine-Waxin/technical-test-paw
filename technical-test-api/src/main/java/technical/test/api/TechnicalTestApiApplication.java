c7cb1bf5cc279ff4f6d0aaaf666d5ad53b227b5f
Pierre-Antoine Waxin <pierreantoine.waxin.partner@decathlon.com>
2024-08-05 09:06:18 +0200

    add necessary dependencies and configuration

--- technical-test-api/src/main/java/technical/test/api/TechnicalTestApiApplication.java
+++ technical-test-api/src/main/java/technical/test/api/TechnicalTestApiApplication.java
@@ -2,8 +2,12 @@ package technical.test.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories(basePackages = "technical.test.api.storage.repositories")
 public class TechnicalTestApiApplication {
 
     public static void main(String[] args) {
